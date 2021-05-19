28101c3180b79778e5c94a7188a1999b1839e455
Douglas <Douglas@Jennifers-MacBook-Pro.local>
2021-05-19 17:33:46 -0400

    additions to recursion respository

---
+++ recursion_2.py
@@ -0,0 +1,16 @@
+houses = ["Gene's house", "Barry's house", "Fuches' house", "Hank's house", "Sally's house"]
+
+def deliver_presents_recursively(houses):
+    if len(houses) ==  1:
+        house = houses[0]
+        print ("Delivering presents to", house)
+
+    else:
+        mid = len(houses)//2
+        first_half = houses[:mid]
+        second_half = houses[mid:]
+
+        deliver_presents_recursively(first_half) 
+        deliver_presents_recursively(second_half)
+
+deliver_presents_recursively(houses)
